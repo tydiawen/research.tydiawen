eef348841c912402e0d71844e3616757952debc7
Tydia wen <tydiayayaya@192.168.1.101>
2025-01-29 18:32:55 +0000

    modified 29.01.2025

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import ImageGrid from '../sections/Posts';
-import TextGrid from '../sections/Captions';
 
 export default function Home() {
   return (
     <main id="main">
       <ImageGrid/>
-      <TextGrid />
     </main>
   );
 }
--- src/sections/Posts.tsx
+++ src/sections/Posts.tsx
@@ -5,44 +5,50 @@ import Image from 'next/image';
 interface ImageItemProps {
   src: string;
   alt: string;
+  text: string;
 }
 
 const ImageItem: React.FC<ImageItemProps> = ({ src, alt }) => (
   <div className="grid-item">
-    <Image
-      src={src}
-      alt={alt}
-      layout="fill"
-      objectFit="cover" />
+    <Image src={src} alt={alt} layout="fill" objectFit="cover" />
   </div>
 );
 
+const images = [
+  { src: "/images/0-cave.jpg", alt: "", text: "0-cave.jpg" },
+  { src: "/images/ones3.jpg", alt: "", text: "Arjan de Nooy 99:1" },
+  { src: "/images/openart-image_aJiLSyxf_1737845001780_raw.jpg", alt: "", text: "openart-image_aJiLSyxf_1737845001780_raw.jpg" },
+  { src: "/images/bcc16.jpg", alt: "", text: "City Lights" },
+  { src: "/images/43.1.03_Eine_Kleine_Nachtmusik_THUMB_from_Tate.jpg", alt: "", text: "Love&Pop" },
+  { src: "/images/663c3a5a1892f9b1708193dadb21cacf.jpg", alt: "", text: "Dorothea Tanning" },
+  { src: "/images/79954.jpg", alt: "", text: "Laurie Simmons" },
+  { src: "/images/IMG_3105 Medium.jpeg", alt: "", text: "Corinne Day" },
+  { src: "/images/Screenshot 2024-11-10 at 9.00.05 PM Medium.jpeg", alt: "", text: "IMG_3105.jpeg" },
+  { src: "/images/screen-shot-2012-01-30-at-13-27-19.png", alt: "", text: "Gondry on Dead Leaves and the Dirty Ground" },
+  { src: "/images/IMG_7704.jpeg", alt: "", text: "IMG_7704.jpeg" },
+  { src: "/images/8541d03c-b4e7-4782-a921-aa729df3b0af-Riverbank.jpeg", alt: "", text: "Riverbank.jpeg" },
+  { src: "/images/Un chien andalou thumb.jpeg", alt: "", text: "Un Chien Andalou" },
+  { src: "/images/Gozan-okuribi_Ho.jpeg", alt: "", text: "Gozan-okuribi_Ho.jpeg" },
+  { src: "/images/IMG_4105.jpg", alt: "", text: "IMG_4105.jpg" },
+  { src: "/images/Dunes.png", alt: "", text: "Dunes" },
+  { src: "/images/bluespring2.png", alt: "", text: "Blue Spring" },
+  { src: "/images/IMG_8441.png", alt: "", text: "IMG_8441.png" }
+];
+
 const ImageGrid = () => {
   return (
-    <div className="grid-container">
-      <ImageItem src="/images/0-cave.jpg" alt="Cave Image" />
-      <ImageItem src="/images/ones3.jpg" alt="Another Image" />
-      <ImageItem src="/images/openart-image_aJiLSyxf_1737845001780_raw.jpg" alt="Another Image" />
-      <ImageItem src="/images/bcc16.jpg" alt="Another Image" />
-      <ImageItem src="/images/43.1.03_Eine_Kleine_Nachtmusik_THUMB_from_Tate.jpg" alt="Another Image"/>
-      <ImageItem src="/images/663c3a5a1892f9b1708193dadb21cacf.jpg" alt="Another Image"/>
-      <ImageItem src="/images/79954.jpg" alt="Another Image"/>
-      <ImageItem src="/images/IMG_3105 Medium.jpeg" alt="Another Image"/>
-      <ImageItem src="/images/Screenshot 2024-11-10 at 9.00.05 PM Medium.jpeg" alt="Another Image"/>
-      <ImageItem src="/images/screen-shot-2012-01-30-at-13-27-19.png" alt="Another Image" />
-      <ImageItem src="/images/IMG_7704.jpeg" alt="Another Image"/>
-      <ImageItem src="/images/8541d03c-b4e7-4782-a921-aa729df3b0af-Riverbank.jpeg" alt="Another Image" />
-      <ImageItem src="/images/Un chien andalou thumb.jpeg" alt="Another Image"/>
-      <ImageItem src="/images/Gozan-okuribi_Ho.jpeg" alt="Another Image"/>
-      <ImageItem src="/images/IMG_4105.jpg" alt="Another Image"/>
-      <ImageItem src="/images/Dunes.png" alt="Another Image" />
-      <ImageItem src="/images/bluespring2.png" alt="Another Image" />
-      <ImageItem src="/images/IMG_8441.png" alt="Another Image" />
-      
-    </div> 
+    <div>
+      {/* ✅ Image Grid */}
+      <div className="grid-container">
+        {images.map((image, index) => (
+          <div key={index} className="grid-item-container">
+            <ImageItem src={image.src} alt={image.alt} text={image.text} />
+            <p className="image-text">{image.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
 
 export default ImageGrid;
-
-
