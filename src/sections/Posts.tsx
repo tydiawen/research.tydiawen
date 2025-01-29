d7c3818aea6d133532caa329ddf8b98f45f866cf
Tydia wen <tydiayayaya@192.168.1.101>
2025-01-29 02:04:44 +0000

    Updated image grid responsiveness and styling

--- src/sections/Posts.tsx
+++ src/sections/Posts.tsx
@@ -12,23 +12,15 @@ const ImageItem: React.FC<ImageItemProps> = ({ src, alt }) => (
     <Image
       src={src}
       alt={alt}
-      layout="fill"  // Make the image fill the container
-      objectFit="cover"  // Ensures the image doesn't stretch and covers the entire area
+      layout="fill"
+      objectFit="cover"
     />
   </div>
 );
 
 const ImageGrid = () => {
   return (
-    <div
-      style={{
-        display: 'grid',
-        gridTemplateColumns: 'repeat(6, 1fr)',  // 6 equal-width columns
-        gap: '15px',  // Space between images
-        padding: '15px', // Padding around the grid
-      }}
-    >
-      {/* Add ImageItem components here, passing the image src and alt */}
+    <div className="grid-container">
       <ImageItem src="/images/0-cave.jpg" alt="Cave Image" />
       <ImageItem src="/images/79954.jpg" alt="Another Image" />
       <ImageItem src="/images/bcc16.jpg" alt="Another Image" />
@@ -39,17 +31,6 @@ const ImageGrid = () => {
       <ImageItem src="/images/IMG_7704.jpeg" alt="Another Image" />
       <ImageItem src="/images/Dunes.png" alt="Another Image" />
       <ImageItem src="/images/Gozan-okuribi_Ho.jpeg" alt="Another Image" />
-      <ImageItem src="/images/0-cave.jpg" alt="Cave Image" />
-      <ImageItem src="/images/79954.jpg" alt="Another Image" />
-      <ImageItem src="/images/bcc16.jpg" alt="Another Image" />
-      <ImageItem src="/images/ones3.jpg" alt="Another Image" />
-      <ImageItem src="/images/IMG_3105.jpg" alt="Another Image" />
-      <ImageItem src="/images/IMG_4105.jpg" alt="Another Image" />
-      <ImageItem src="/images/Un chien andalou thumb.jpeg" alt="Another Image" />
-      <ImageItem src="/images/IMG_7704.jpeg" alt="Another Image" />
-      <ImageItem src="/images/Dunes.png" alt="Another Image" />
-      <ImageItem src="/images/Gozan-okuribi_Ho.jpeg" alt="Another Image" />
-      {/* Add more ImageItem components as needed */}
     </div>
   );
 };
