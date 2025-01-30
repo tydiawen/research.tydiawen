0ca76a9956c219c6822598ad91a2932c7379aefe
Tydia wen <tydiayayaya@192.168.1.100>
2025-01-30 21:23:28 +0000

    Added Works link in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className="header">
-       <h1>tydiawen.com<span className="underline">__research</span></h1>
+       <h1>tydiawen.com<span className="underline">research
+       <Link href="/works" className="works-link">work</Link></span></h1>
     </header>
   );
 };
